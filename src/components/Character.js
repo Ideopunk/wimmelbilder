4f698c00d8510be487048ccd09ce4e3cfc2c08c9
Conor Barnes <conorbarnes93@gmail.com>
2020-10-18 12:24:45 -0700

    transparency on find character

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,12 +3,15 @@ import ASH from "../assets/Ash_trans.png";
 import CC from "../assets/Candy_Cane.png";
 import SNAIL from "../assets/Waving_Snail.png";
 
-const Character = () => {
+const Character = (props) => {
+      const {find} = props
+
+
 	return (
 		<div>
-			<img className="character" src={ASH} alt="current character" />
-			<img className="character" src={CC} alt="current character" />
-			<img className="character" src={SNAIL} alt="current character" />
+			<img className={`character ${find[0]? "found" : ""}`} src={ASH} alt="current character" />
+			<img className={`character ${find[1]? "found" : ""}`} src={CC} alt="current character" />
+			<img className={`character ${find[2]? "found" : ""}`} src={SNAIL} alt="current character" />
 		</div>
 	);
 };
