bdf9fc374d5b853906aea98afdea9431f278f5a7
Conor Barnes <conorbarnes93@gmail.com>
2020-10-17 20:10:12 -0700

    AT handleclick for getting percent coordinates

--- src/components/AT.js
+++ src/components/AT.js
@@ -1,17 +1,27 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import WIMMELBILDER from "../assets/adventuretime.png";
 
-const AT = (props)  => {
+const AT = (props) => {
+	const handleClick = (e) => {
+		// get coordinates
+		const realX = e.clientX - e.target.getBoundingClientRect().x;
+		const realY = e.clientY - e.target.getBoundingClientRect().y;
+		const percentX = realX / e.target.getBoundingClientRect().width;
+		const percentY = realY / e.target.getBoundingClientRect().height;
+		console.log(percentX, percentY);
+		// pass them to app to check against cloud function
+	};
 
-    const handleClick = (e) => {
-        console.log(e.target)
-        // get coordinates
-        // pass them to app to check against cloud function
-    }
-
-    return (
-        <img className="image" src={WIMMELBILDER} alt="adventure time wimmelbilder" onClick={(e) => handleClick(e)}/>
-    )
-}
+	return (
+		<div className="image-container">
+			<img
+				className="image"
+				src={WIMMELBILDER}
+				alt="adventure time wimmelbilder"
+				onClick={(e) => handleClick(e)}
+			/>
+		</div>
+	);
+};
 
 export default AT;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,5 +1,4 @@
 import React from "react";
-import WIMMELBILDER from "../assets/adventuretime.png";
 import ASH from "../assets/Ash_trans.png";
 import CC from "../assets/Candy_Cane.png";
 import SNAIL from "../assets/Waving_Snail.png";
